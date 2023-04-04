4d7dfeba3495200188fb95e585eb8cda8c432185
lazyloading <lazyloading@163.com>
2023-04-04 19:31:46 +0800

    feat: 初始化项目，调用接口

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Lazyloading on 2023/4/3.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ChatView()
+        }
+    }
+}
